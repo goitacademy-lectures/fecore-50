35547189b0d9f06206f55d58d4ebfd251d0df897
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2022-04-22 21:34:49 +0300

    add mobile-modal element

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const body = document.querySelector("body");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -8,5 +9,6 @@
     menuBtnRef.classList.toggle("is-active");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
+    body.classList.toggle("no-scroll");
   });
 })();
